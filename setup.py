c617a8d6629ebec90d01862daaa70fc99f914454
lexual <lex@jbadigital.com>
2013-05-18 17:22:23 +1000

    setup.py fixed.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
-import pandas_love_ponies
 
 
 with open('requirements.txt') as f:
     requires = [l.rstrip() for l in f if not l.startswith('#')]
-
+    version = '0.5.0'
 
 
 setup(
@@ -13,8 +12,8 @@ setup(
         description='Write Pandas dataframes to Django models',
         long_description=open('README.md').read(),
         url='https://github.com/lexual/pandas-love-ponies',
-        version=pandas_love_ponies.__version__,
-        packages=['pandas_love_ponies',],
+        version=version,
+        packages=['pandas_love_ponies'],
         license='BSD',
         install_requires=requires
 )
